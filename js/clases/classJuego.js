440482d848bd49fa5260f77490a2ab44f0ca9db5
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-12-05 09:26:49 -0300

    se realizo el constructor de la clase de Juego

--- js/clases/classJuego.js
+++ js/clases/classJuego.js
@@ -6,6 +6,16 @@ class Juego{
     #imagenURL;
     #descripcion;
     #requisitos;
-    #desarrolador;
+    #desarrollador;
     #reseñas;
+    constructor(codigo = uuidv4(), nombre, precio, categoria, imagen, descripcion, requisitos, desarrollador){
+        this.#codigo = codigo;
+        this.#nombre = nombre;
+        this.#precio = precio;
+        this.#categoria = categoria;
+        this.#imagenURL = imagen;
+        this.#descripcion = descripcion;
+        this.#requisitos = requisitos;
+        this.#desarrollador = desarrollador;
+    }
 }
