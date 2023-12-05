91c0dd8160e388a28d59bc9d1a161bed012da7b4
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-12-05 09:17:08 -0300

    se agregaron las propiedades correspondientes a la clase usuario

--- js/clases/classUsuario.js
+++ js/clases/classUsuario.js
@@ -2,10 +2,12 @@ class Usuario{
     #usuario;
     #correo;
     #contrasenia;
-    constructor(usuario, correo, contrasenia){
+    #rol;
+    constructor(usuario, correo, contrasenia, rol){
         this.#usuario = usuario;
         this.#correo = correo;
         this.#contrasenia = contrasenia;
+        this.#rol = rol;
     }
 
     get usuario(){
@@ -30,5 +32,13 @@ class Usuario{
 
     set contrasenia(clave){
         this.#contrasenia = clave;
+    }
+
+    get rol(){
+        return this.#rol;
+    }
+
+    set rol(newRol){
+        this.#rol = rol;
     }
 }
