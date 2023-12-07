6db83f72bc021761c493f1461388a9995b66546a
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-12-06 21:15:52 -0300

    se creo la lógica para crear un usuario y agregarlo en el localStorage

--- js/userAdmin.js
+++ js/userAdmin.js
@@ -1,11 +1,40 @@
-import Usuario from './clases/classUsuario.js';
+import Usuario from "./clases/classUsuario.js";
+import {
+  validarTexto,
+  validarContrasenia,
+  validarEmail,
+  validarCheckBox,
+} from "./formValidation.js";
 
-const email = document.querySelector('#correoRegister'),
-        contrasenia = document.querySelector('#contraseniaRegister'),
-        contrasenia2 = document.querySelector('#contraseniaRegisterConfirm'),
-        termCondiciones = document.querySelector('#checkboxRegister');
+import { insertLocalStorage } from './dataStorageManager.js';
+import {limpiarFormularios} from './auxiliarFunctions.js';
+
+const email = document.querySelector("#correoRegister"),
+        username = document.querySelector('#username'),
+        contrasenia = document.querySelector("#contraseniaRegister"),
+        contrasenia2 = document.querySelector("#contraseniaRegisterConfirm"),
+        termCondiciones = document.querySelector("#checkboxRegister");
+
+const formRegister = document.querySelector("#formRegister");
+const usuarios = [];
 
 const crearUsuario = (e) => {
-    e.preventDefault();
-    const usuario = new Usuario()
-}
+  e.preventDefault();
+  if (
+    validarContrasenia(contrasenia) &&
+    validarContrasenia(contrasenia2) &&
+    validarTexto(email, 10, 320) &&
+    validarTexto(username, 3, 50) &&
+    validarEmail(email) &&
+    validarCheckBox(termCondiciones)
+  ) {
+    if(contrasenia.value === contrasenia2.value){
+        const user = new Usuario(undefined, username.value, email.value, contrasenia.value, "invitado");
+        usuarios.push(user);
+        insertLocalStorage('usuarios', usuarios);
+        limpiarFormularios(formRegister);
+    }
+  }
+};
+
+formRegister.addEventListener('submit', crearUsuario);
