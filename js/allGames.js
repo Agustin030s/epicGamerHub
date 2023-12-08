eb72ec8c7dcf90b9f8490159de3c904ed545f5e5
Martin Cristian Cornejo <martincckd130@gmail.com>
2023-12-08 18:33:04 -0300

    Termine la funcion para agregar los juegos dentro de la pagina de todos los juegos

--- js/allGames.js
+++ js/allGames.js
@@ -1,1 +1,12 @@
+import {mostrarJuegos} from "./auxiliarFunctions.js";
+import {getLocalStorage} from "./dataStorageManager.js";
+
+const listaDeJuegos = getLocalStorage('juegos');
 const contenedorJuegos = document.getElementById('gamesContainer');
+
+// Mostramos loe juegos en la pagina 
+mostrarJuegos(listaDeJuegos,listaDeJuegos.length,contenedorJuegos);
+
+window.verDetalle = (codigoDeJuego) => {
+    window.location.href = './detalleJuego.html?codigo=' + codigoDeJuego;
+}
