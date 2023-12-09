7f03459eb79d125a282db63f3e303a2c5525707c
Martin Cristian Cornejo <martincckd130@gmail.com>
2023-12-09 13:11:39 -0300

    Finalizada la lógica para buscar juegos por texto en la página de todos los juegos

--- js/allGames.js
+++ js/allGames.js
@@ -3,6 +3,8 @@ import {getLocalStorage} from "./dataStorageManager.js";
 
 const listaDeJuegos = getLocalStorage('juegos');
 const contenedorJuegos = document.getElementById('gamesContainer');
+const buscarJuegos = document.getElementById('searchGames');
+const inputJuego = document.getElementById('inputGames');
 
 // Mostramos todos los juegos guardados en el localStorage
 mostrarJuegos(listaDeJuegos,listaDeJuegos.length,contenedorJuegos);
@@ -10,3 +12,24 @@ mostrarJuegos(listaDeJuegos,listaDeJuegos.length,contenedorJuegos);
 window.verDetalle = (codigoDeJuego) => {
     window.location.href = './detalleJuego.html?codigo=' + codigoDeJuego;
 }
+
+const busquedaDeJuegos = (e) => {
+    e.preventDefault();
+    const juegoBuscado = listaDeJuegos.filter((juego) => juego.nombre.includes(inputJuego.value));
+    let mensaje = `<h3 class="order-first d-block text-center text-light fs-1 mb-5">Se muestran resultados de busqueda de '${inputJuego.value}'</h3>`;
+
+    if (juegoBuscado.length !== 0) {
+        if (juegoBuscado.length === listaDeJuegos.length) {
+            // En caso que el usuario pulse buscar con el input vacio, se mostraran todos los juegos sin el mensaje 
+            mostrarJuegos(listaDeJuegos,listaDeJuegos.length,contenedorJuegos);
+        } else {
+            mostrarJuegos(juegoBuscado,juegoBuscado.length,contenedorJuegos);
+            contenedorJuegos.innerHTML += mensaje;
+        }
+    } else {
+        contenedorJuegos.innerHTML = `<p class="text-center text-secondary display-6">No se encontro el juego buscado</p>`
+        contenedorJuegos.innerHTML += mensaje;
+    }
+}
+
+buscarJuegos.addEventListener('submit',busquedaDeJuegos);
